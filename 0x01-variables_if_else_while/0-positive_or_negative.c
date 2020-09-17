045f161093b8a96b0b04826adf27b0ef1f06bffa
stilop1 <stilopez0816@outlook.com>
2020-09-17 23:29:43 +0000

    learning if and else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there
+/**
+* main - Entry point
 *
-*main - Entry point
+* betty style doc for function main goes there
 *
-*betty style doc for function main goes there
-*
-*Return: Always 0 (Success)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
