429c9a7d70caf8345c39df49a1cf1481b29de51b
stilop1 <stilopez0816@outlook.com>
2020-09-19 04:05:58 +0000

    learning

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * betty style doc for function main goes there
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for ( y = '0'; y <= '9'; y++)
+		{
+			putchar(x);
+			putchar(y);
+			if (x == '9' && y == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
