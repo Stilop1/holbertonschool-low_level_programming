674979c4a075e3bfd5b27541e1035a88f9637700
stilop1 <stilopez0816@outlook.com>
2020-09-19 01:36:15 +0000

    using putchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * betty style doc for function main goes there
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
