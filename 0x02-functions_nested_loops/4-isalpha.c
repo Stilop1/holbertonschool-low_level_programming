e4be5920c60d88adf1d8753b4707deb94ec2f996
stilop1 <stilopez0816@outlook.com>
2020-09-23 05:03:31 +0000

    learning

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 
 /**
- * _islower - Write a function that checks for alphabetic character.
+ * _isalpha - Write a function that checks for alphabetic character.
  * @c: The character to print
  *
  * Return: Always 0.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _islpha(int c)
+int _isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
-	if else(c >= 'A' && c <= 'z')
+	else if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
