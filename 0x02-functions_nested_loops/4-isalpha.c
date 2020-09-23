12e2d7a5b75932a59fd02f6bf905f920290d1b96
stilop1 <stilopez0816@outlook.com>
2020-09-23 03:50:26 +0000

    learning

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _islower - writes the character c to stdout
+ *@c: The character to print
+ *
+ * Return: Always 0.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _islpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	     	return (1);
+	if else (c >= 'A' && c <== 'z')
+			retun (1);
+	else
+		return (0);
+}
