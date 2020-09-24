42d70efe374381142017c3f13e868b58b48c0267
stilop1 <stilopez0816@outlook.com>
2020-09-24 22:40:58 +0000

    leaninig

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int x;
+	int y;
+	
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9 && y <= 14)
+				_putchar((y / 10) + 48);
+				_putchar((y % 10) + 48);
+		}
+		_putchar('\n');
+	 }
+}
