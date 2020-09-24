35733d2d52214e759da58255b0a0f991bbb3b8d3
stilop1 <stilopez0816@outlook.com>
2020-09-24 20:43:23 +0000

    learning

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * _isdigit - check the code for Holberton School students.
  * @c: is number
  *
  * Return: Always 0.
