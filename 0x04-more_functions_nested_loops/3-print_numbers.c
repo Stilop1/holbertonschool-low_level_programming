b290a9c4c57b5cc912249f771f4ae4ba2c6d0bf2
stilop1 <stilopez0816@outlook.com>
2020-09-24 21:22:13 +0000

    learning

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		_putchar(x);
+	}	
+	_putchar('\n');
+}
+
