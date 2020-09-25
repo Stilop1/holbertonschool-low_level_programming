29189a0aef4acb136efd43ccfe2eb9312409ca10
stilop1 <stilopez0816@outlook.com>
2020-09-25 04:32:39 +0000

    learning

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 1; x <= 100; x++)
+	{
+		y = x % 3;
+		z = x % 5;
+		if (y == 0 && z == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (z == 0)
+			printf("Buzz");
+		else if (y == 0)
+			printf("Fizz");
+		else
+			printf("%d", x);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
