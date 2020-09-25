d464131542eb31d1ad2e86f1c1fa62082734810a
stilop1 <stilopez0816@outlook.com>
2020-09-25 04:36:08 +0000

    learning

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,15 +16,14 @@ int main(void)
 		y = x % 3;
 		z = x % 5;
 		if (y == 0 && z == 0)
-		{
 			printf("FizzBuzz");
-		}
 		else if (z == 0)
 			printf("Buzz");
 		else if (y == 0)
 			printf("Fizz");
 		else
 			printf("%d", x);
+		if (x < 100)
 		printf(" ");
 	}
 	printf("\n");
