45b14ce1b00554e4745f57fe6e95ccb512b8d7f9
stilop1 <stilopez0816@outlook.com>
2020-09-25 02:22:40 +0000

    learning

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_square - check the code for Holberton School students.
+ * @size: is the size of the square.
+ *
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
