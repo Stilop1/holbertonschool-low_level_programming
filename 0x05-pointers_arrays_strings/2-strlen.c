ac0840bc8cc10ff75a4c87e96c03fce7f73e3670
Stilop1 <stilopez0816@gmail.com>
2020-09-29 22:35:10 +0000

    learning

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - the length of a string.
+ * @s:  a string
+ *
+ * Return: the length of a string. 
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+	}	
+	return (x);
+}
