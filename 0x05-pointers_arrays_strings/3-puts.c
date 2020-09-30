b0287f115590bee549bfdc7ccafad73ac3e2ff31
Stilop1 <stilopez0816@gmail.com>
2020-09-30 02:50:16 +0000

    learning

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * void _puts - prints a string, followed by a new line
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int x;
+	
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		_putchar(str[x]);
+	}	
+	_putchar('\n');
+}
